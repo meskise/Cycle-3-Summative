3c5039b030590d84779bd7cc298cead600a37257
meskise <meskise@limestone.on.ca>
2022-05-30 10:50:15 -0400

    Added Protective Orbs around player
    Protective Orbs spawn around player when "C" is pressed

--- Cycle 3 Summative - Eric Meskis/MyWorld.java
+++ Cycle 3 Summative - Eric Meskis/MyWorld.java
@@ -8,14 +8,28 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
     public MyWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        spawnObjects();
     }
+    
+    public void act()
+    {
+        
+    }
+    
+    public void spawnObjects()
+    {
+        Woodcutter woodcutter = new Woodcutter();
+        addObject (woodcutter, 250, 250);
+    }
+    
+    
+    
 }
--- Cycle 3 Summative - Eric Meskis/ProtectiveOrb.java
+++ Cycle 3 Summative - Eric Meskis/ProtectiveOrb.java
@@ -8,12 +8,21 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class ProtectiveOrb extends Actor
 {
+    
+    public ProtectiveOrb()
+    {
+        GreenfootImage image = getImage();
+        image.scale(15, 15);
+        setImage(image);
+    }
+    
     /**
      * Act - do whatever the ProtectiveOrb wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
-    }    
+       // Act.
+    }
+    
 }
